f4f65cd123da512a788349c27ce88cdb496fbac1
Fan Qu <fan_qu@icloud.com>
2018-11-24 16:00:53 +0800

    add is_a to messageInfo

--- Car-Pilot-master/FindRoad.h
+++ Car-Pilot-master/FindRoad.h
@@ -60,7 +60,8 @@ struct _MessageInfo{
     int xs_pos[5], ys_pos[5]; //乘客的起点x坐标和y坐标
     int xe_pos[5], ye_pos[5];
     int my_x, my_y, oppo_x, oppo_y; //我的x,y坐标和对手坐标
-    char pass_status[8]; //乘客状态0为未上车，1为在我的车,2为在对手车
+    char pass_status[7]; //乘客状态0为未上车，1为在我的车,2为在对手车
+    char is_a; //当is_a为0时我的车是B车,当is_a==1时我的车是A车
 };
 
 typedef struct _MessageInfo MessageInfo;
